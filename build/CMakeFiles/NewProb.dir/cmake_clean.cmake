2138f18a75fedf9e8c04d83e48c5b252ab527ad1
SERGEI <sergeitimofeev76@gmail.com>
2020-12-19 12:33:00 +0300

    начало дня 19.12.20

---
+++ build/CMakeFiles/NewProb.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NewProb.dir/src/main.cpp.o"
+  "NewProb"
+  "NewProb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NewProb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sergei/OP_GL_Project/NewProb
+# Build directory: /home/sergei/OP_GL_Project/NewProb/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
